4d79da4ca37d791f59602c85807788b7c733acb4
luisnhabangue <luisnhabangue@uem.ac.mz>
2026-09-20 16:28:11 +0200

    Nao estava a rodar a view

--- src/view/Main.java
+++ src/view/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
